bf99df7676e2847af852029a5e5fccd82ef708e8
marcosdrey <programmarcos05@gmail.com>
2025-03-06 18:42:59 -0300

    Add logging to the DataTransformer class

--- src/transformation/data_transformer.py
+++ src/transformation/data_transformer.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 class DataTransformer:
@@ -8,22 +14,27 @@ class DataTransformer:
         self.df = self.__load_data()
 
     def __load_data(self):
+        logging.info("Carregando dados do CSV...")
         return pd.read_csv(self.raw_data_path)
 
     def __fix_goals_difference(self):
+        logging.info("Corrigindo saldo de gols...")
         self.df["goals_difference"] = self.df.goals_scored - self.df.goals_conceded
 
     def __change_names(self, hash_names: dict | None):
         if isinstance(hash_names, dict):
+            logging.info("Padronizando nomes dos times...")
             self.df["name"] = self.df["name"].replace(hash_names)
 
     def __create_winning_percentage_column(self):
+        logging.info("Calculando percentual de vitórias...")
         winning_percentage_series = (
             (self.df["points"] / (self.df["total_games"] * 3)) * 100
         ).round(2)
         self.df["winning_percentage"] = winning_percentage_series
 
     def __save_data(self):
+        logging.info("Salvando dados transformados...")
         self.df.to_csv(self.save_data_path, index=False)
 
     def run(self, hash_names: dict | None):
