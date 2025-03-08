d8f59a36edbd975d8bce70f20aaeb83309341f76
marcosdrey <programmarcos05@gmail.com>
2025-03-08 12:31:55 -0300

    Translate data_transformer logging infos

--- src/transformation/data_transformer.py
+++ src/transformation/data_transformer.py
@@ -14,27 +14,27 @@ class DataTransformer:
         self.df = self.__load_data()
 
     def __load_data(self):
-        logging.info("Carregando dados do CSV...")
+        logging.info("Loading data...")
         return pd.read_csv(self.raw_data_path)
 
     def __fix_goals_difference(self):
-        logging.info("Corrigindo saldo de gols...")
+        logging.info("Correcting goal difference...")
         self.df["goals_difference"] = self.df.goals_scored - self.df.goals_conceded
 
     def __change_names(self, hash_names: dict | None):
         if isinstance(hash_names, dict):
-            logging.info("Padronizando nomes dos times...")
+            logging.info("Standardising team names...")
             self.df["name"] = self.df["name"].replace(hash_names)
 
     def __create_winning_percentage_column(self):
-        logging.info("Calculando percentual de vitórias...")
+        logging.info("Calculating winning percentage...")
         winning_percentage_series = (
             (self.df["points"] / (self.df["total_games"] * 3)) * 100
         ).round(2)
         self.df["winning_percentage"] = winning_percentage_series
 
     def __save_data(self):
-        logging.info("Salvando dados transformados...")
+        logging.info("Saving transformed data...")
         self.df.to_csv(self.save_data_path, index=False)
 
     def run(self, hash_names: dict | None):
