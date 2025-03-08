ebcc1d82f82600909b34bc3fa26212788dfb3b5d
marcosdrey <programmarcos05@gmail.com>
2025-03-08 12:29:46 -0300

    Translate database_loader logging infos

--- src/loading/database_loader.py
+++ src/loading/database_loader.py
@@ -13,26 +13,26 @@ logging.basicConfig(
 class DatabaseLoader:
 
     def __enter__(self):
-        logging.info("Conectando ao banco de dados...")
+        logging.info("Connecting to the database...")
         self.__conn = self.__connect_to_database()
         self.__cur = self.__conn.cursor()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        logging.info("Encerrando conexão ao banco de dados...")
+        logging.info("Closing the database connection...")
         self.__cur.close()
         self.__conn.close()
 
     def create_table(self, query):
-        logging.info("Criando tabela (ou apenas pegá-la caso já exista)...")
+        logging.info("Creating a table (or just picking it up if it already exists)...")
         self.__cur.execute(query)
         self.__conn.commit()
 
     def insert_data(self, query, data):
-        logging.info("Inserindo dados...")
+        logging.info("Inserting data...")
         self.__cur.executemany(query, data)
         self.__conn.commit()
-        logging.info(f"{self.__cur.rowcount} dados inseridos na tabela.")
+        logging.info(f"{self.__cur.rowcount} data inserted in the table.")
 
     def __connect_to_database(self):
         return psycopg2.connect(
